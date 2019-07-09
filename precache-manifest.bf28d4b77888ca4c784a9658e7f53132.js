1270353f7b84d6389ddefb1412455b28e7f445fd
Koen Corneillie <koen.corneillie@be00600.tbwagroup.be>
2019-07-09 17:16:46 +0200

    Updates

---
+++ precache-manifest.bf28d4b77888ca4c784a9658e7f53132.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf3e9b7534fe492d5addd84f13c1ebf6",
+    "url": "/jacky/index.html"
+  },
+  {
+    "revision": "6968e3fd11fcc2ec5b48",
+    "url": "/jacky/static/css/main.5599f4ad.chunk.css"
+  },
+  {
+    "revision": "b21a5dafb7bf3690877f",
+    "url": "/jacky/static/js/2.f5883e4a.chunk.js"
+  },
+  {
+    "revision": "6968e3fd11fcc2ec5b48",
+    "url": "/jacky/static/js/main.2d985fab.chunk.js"
+  },
+  {
+    "revision": "489f5f24013574215f13",
+    "url": "/jacky/static/js/runtime~main.3b750834.js"
+  }
+]);
